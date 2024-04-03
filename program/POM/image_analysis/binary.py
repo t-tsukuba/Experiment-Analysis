5ee9f35dd219fe31445b8d5cdeab7e2a5ecd4276
t-tsukuba <2002tatsuki0323@gmail.com>
2024-04-03 14:44:40 +0000

    画像を読み込み白黒にする関数の実装

--- program/POM/image_analysis/binary.py
+++ program/POM/image_analysis/binary.py
@@ -1,8 +1,22 @@
 import cv2
 
 
-def grayscale_to_binary(input_filepath, threshold_value=90):
-    src = cv2.imread(input_filepath)
-    _, gray_th = cv2.threshold(src, threshold_value, 255, cv2.THRESH_BINARY)
+def img_to_grayscasle(input_filepath):
+    raw_img = cv2.imread(input_filepath)
+    gray_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
 
-    return gray_th
+    return gray_img
+
+
+def grayscale_to_binary(gray, th):
+    # 閾値を設定して二値化
+    _, binary_img = cv2.threshold(gray, th, 255, cv2.THRESH_BINARY)
+
+    return binary_img
+
+
+def img_to_binary(input_filepath, threshold):
+    gray_img = img_to_grayscasle(input_filepath)
+    binary_img = grayscale_to_binary(gray_img, threshold)
+
+    return binary_img
