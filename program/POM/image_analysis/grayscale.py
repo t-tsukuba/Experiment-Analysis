40b53b47d70a19a6271b0e484d510296708fd472
t-tsukuba <152946066+t-tsukuba@users.noreply.github.com>
2024-04-03 23:53:22 +0900

    画像を読み込み白黒に二値化し画像出力 (#6)
    * グレースケール画像を二値化する関数の実装
    
    * グレースケール処理の関数を削除、画像を白黒出力のコードを
    削除
    
    * 画像を読み込み白黒にする関数の実装
    
    * 画像を読み込み白黒にする関数を実行
    
    * 出力のクリア

--- program/POM/image_analysis/grayscale.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-
-def img_to_grayscasle(input_filepath):
-    src = cv2.imread(input_filepath)
-    cvtcolor_result = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-
-    return cvtcolor_result
